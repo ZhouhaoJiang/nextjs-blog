7b3727f46cbd1318779c9ef1da41f72e4493deb0
Joe <1264204425@qq.com>
2023-09-24 19:20:20 +0800

    fist commit

---
+++ next.config.js
@@ -0,0 +1,15 @@
+// next.config.js
+
+// module.exports = {
+//     future: {
+//         webpack5: true,
+//     },
+//     webpack(config) {
+//         config.resolve.fallback = {
+//             ...config.resolve.fallback,
+//             fs: false,
+//         };
+//
+//         return config;
+//     },
+// };
